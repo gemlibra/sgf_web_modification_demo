fa3396f4e659473dc5964a136095f42d59f83993
gemlibra <93558651+gemlibra@users.noreply.github.com>
2022-02-27 16:01:49 -0500

    Add files via upload

---
+++ js/jqsgfwebproject.js
@@ -0,0 +1,22 @@
+
+
+
+//$(document).ready(function(){
+//    $('#iframemap').hide()
+//    $("#iframemap").slideDown("slow");
+   // $('#iframemap').toggle('slow');
+//}) 
+
+$(document).ready(function(){
+    $("iframemap").fadeIn(10000);
+})
+
+
+//$(document).ready(function() {
+//   $("#iframemap").hide();
+// });
+                           //  {
+//    $("#iframemap").show( "slide", {direction: "up" }, 10000 );
+//};
+        
+
